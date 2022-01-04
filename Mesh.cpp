398187bcb9308839da084d88714dd82d692687cf
Xav-la-program <59728500+Xav-la-program@users.noreply.github.com>
2022-01-04 19:18:08 +0100

    Update Mesh.cpp
    hand made modif

--- Mesh.cpp
+++ Mesh.cpp
@@ -34,7 +34,7 @@ namespace mesh
         return res;
     }
 
-    void Mesh::run()
+    std::vector<std::vector<double>> Mesh::main_run()
     {
         std::vector<double> log_spot_axis = initiate_spot_values(m_S0, m_sigma, m_maturity, m_nb_steps_space);
         std::vector<double> spot_axis;
@@ -53,7 +53,7 @@ namespace mesh
         m_dt = m_maturity / m_nb_steps_time;
 
 
-        grid_res.push_back(Xt1); //à rajouter, car la première colonne doit etre le payoff
+        grid_res.push_back(Xt1); //Ã  rajouter, car la premiÃ¨re colonne doit etre le payoff
 
         for (int i=0; i<m_nb_steps_time; ++i)
         {
@@ -79,71 +79,101 @@ namespace mesh
 //        return grid_matrix;
     }
 
-    double Mesh::get_price()
+    double Mesh::get_price(bool bumped)
     {
-        if (m_nb_steps_space % 2 == 1)
+        if (bumped)
         {
-            return grid_res[m_nb_steps_time - 1][floor(m_nb_steps_space / 2)];
+            return get_price(grid_res_bumped_sigma);
         }
         else
         {
-            // approximation if parameters given by the user are not perfect
-            return (grid_res[m_nb_steps_time - 1][m_nb_steps_space / 2] + grid_res[m_nb_steps_time - 1][m_nb_steps_space / 2 - 1]) / 2;
+            return get_price(grid_res);
         }
+    }
+
+    double Mesh::get_price(std::vector<std::vector<double>> grid)
+    {
+        if (m_nb_steps_space % 2 == 1)
+            {
+                return grid[m_nb_steps_time - 1][floor(m_nb_steps_space / 2)];
+            }
+            else
+            {
+                // interpolation if parameters given by the user are not perfect
+                double denom = std::exp(m_dx) - std::exp(-m_dx);
+                double w1 = (std::exp(m_dx) - 1) / denom;
+                double w2 = (1 - std::exp(-m_dx)) / denom;
+                return grid[m_nb_steps_time - 1][m_nb_steps_space / 2 - 1] * w1 + grid[m_nb_steps_time - 1][m_nb_steps_space / 2] * w2;
+            }
     }
 
     double Mesh::get_delta()
     {
+        double denom = m_S0 * (std::exp(m_dx) - std::exp(-m_dx));
+        double num;
         if (m_nb_steps_space % 2 == 1)
         {
-            return (grid_res[m_nb_steps_time - 1][floor(m_nb_steps_space / 2) + 1] - grid_res[m_nb_steps_time - 1][floor(m_nb_steps_space / 2) - 1]) / (2 * m_dx);
+            int index = floor(m_nb_steps_space / 2);
+            num = grid_res[m_nb_steps_time - 1][index - 1] - grid_res[m_nb_steps_time - 1][index + 1];
         }
         else
         {
-            // approximation if parameters given by the user are not perfect
-            return (grid_res[m_nb_steps_time - 1][m_nb_steps_space / 2 ] - grid_res[m_nb_steps_time - 1][m_nb_steps_space / 2 - 1]) / m_dx;
+            int index = m_nb_steps_space / 2;
+            num = grid_res[m_nb_steps_time - 1][index - 1] - grid_res[m_nb_steps_time - 1][index];
         }
+        return num / denom;
     }
 
     double Mesh::get_gamma()
     {
+        double h1 = m_S0 * (1 - std::exp(-m_dx));
+        double h2 = m_S0 * (std::exp(m_dx) - 1);
+        double denom = h1 * h2 * (h2 + h1);
+        double num;
         if (m_nb_steps_space % 2 == 1)
         {
-            return (grid_res[m_nb_steps_time - 1][floor(m_nb_steps_space / 2) + 2] - grid_res[m_nb_steps_time - 1][floor(m_nb_steps_space / 2) - 2]) / pow(2. * m_dx, 2.);
+            int index = floor(m_nb_steps_space / 2);
+            num = h1 * grid_res[m_nb_steps_time - 1][index - 1] - (h1 + h2) * grid_res[m_nb_steps_time - 1][index] + h2 * grid_res[m_nb_steps_time - 1][index + 1];
         }
         else
         {
-            // approximation if parameters given by the user are not perfect
-            return (grid_res[m_nb_steps_time - 1][m_nb_steps_space / 2 + 1] - grid_res[m_nb_steps_time - 1][m_nb_steps_space / 2 - 2]) / pow(2. * m_dx, 2.);
+            int index = m_nb_steps_space / 2;
+            num = h1 * grid_res[m_nb_steps_time - 1][index - 1] - (h1 + h2) * get_price() + h2 * grid_res[m_nb_steps_time - 1][index];
         }
+        return num / denom;
     }
 
     double Mesh::get_theta()
     {
+        double dt = m_maturity * 365 / m_nb_steps_time; // = 1 / dt
         if (m_nb_steps_space % 2 == 1)
         {
-            return (grid_res[m_nb_steps_time - 1][floor(m_nb_steps_space / 2)] - grid_res[m_nb_steps_time - 2][floor(m_nb_steps_space / 2)]) / m_dt;
+            return (grid_res[m_nb_steps_time - 2][floor(m_nb_steps_space / 2)] - get_price()) / dt;
         }
         else
         {
             // approximation if parameters given by the user are not perfect
-            return ((grid_res[m_nb_steps_time - 1][m_nb_steps_space / 2] - grid_res[m_nb_steps_time - 1][m_nb_steps_space / 2 - 1]) - (grid_res[m_nb_steps_time - 2][m_nb_steps_space / 2] + grid_res[m_nb_steps_time - 2][m_nb_steps_space / 2 - 1])) / m_dt;
+            int index = m_nb_steps_space / 2;
+            double denom = std::exp(m_dx) - std::exp(-m_dx);
+            double w1 = (std::exp(m_dx) - 1) / denom;
+            double w2 = (1 - std::exp(-m_dx)) / denom;
+            double price_t1 = grid_res[m_nb_steps_time - 2][index - 1] * w1 + grid_res[m_nb_steps_time - 2][index] * w2;
+            return (price_t1 - get_price()) / dt;
         }
     }
 
-    double Mesh::get_vega(double bumbed_sigma)
+    double Mesh::get_vega()
     {
-        if (vega_computed)
+        if (!vega_computed)
         {
-            // compute vega using already computed second pricing
-            return 2.0;
-        }
-        else
-        {
-            // launch pricing with bumbed sigma and compute vega
+            // relaunch pricing with bumped vol
+            m_sigma = m_sigma + 0.01;
+            run(true);
             vega_computed = true;
-            return 0;
         }
+        return get_price(true) - get_price(); // 1% vol move
+
     }
 }
+
 
